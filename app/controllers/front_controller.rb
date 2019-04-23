7f08957d726bf0d6f1aaa336045efcf821f95e81
Dássone <dyotamo@gmail.com>
2019-04-23 15:42:45 +0200

    SyntaxError fixing.

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -16,7 +16,7 @@ class FrontController < ApplicationController
 
     # Evaluate the query time
     @exc = Benchmark.measure {
-        rs = model.where("description like ?", "%" + query + "%"))
+        rs = model.where("description like ?", "%" + query + "%")
     }
 
     rs.paginate(page: params[:page], per_page: page_number)
