415416ed69c347c33dd2501e334316295f436e5a
Dássone <dyotamo@gmail.com>
2019-04-23 14:20:12 +0200

    Second chapter added.

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,10 +1,11 @@
 class FrontController < ApplicationController
-  def home
-  end
-
   def search
-    @q = params[:q]
-    redirect_to root_path if @q.nil? || @q.empty?
+    q = params[:q]
+
+    # Sanitize
+    @q = q.strip if !q.nil?
+
+    redirect_to root_path if @q.empty?
     @results = full_search(Company, @q, 10)
   end
 
@@ -13,9 +14,12 @@ class FrontController < ApplicationController
   def full_search(model, query, page_number)
     rs = model.none
 
-    model.column_names.each do |column|
-      rs = rs.or(model.where(column + " like ?", "%" + query + "%"))
-    end
+    # Evaluate the query time
+    @exc = Benchmark.measure {
+      model.column_names.each do |column|
+        rs = rs.or(model.where(column + " like ?", "%" + query + "%"))
+      end
+    }
 
     rs.paginate(page: params[:page], per_page: page_number)
   end
