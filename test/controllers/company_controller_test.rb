72ba885d9f5b853d5a2abe2441ba772b71cff85c
Dássone <dyotamo@gmail.com>
2019-06-19 14:21:44 +0200

    New style and ignoring tests for a while...

--- test/controllers/company_controller_test.rb
+++ test/controllers/company_controller_test.rb
@@ -1,9 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class CompanyControllerTest < ActionDispatch::IntegrationTest
-  test "should get view" do
-    get company_view_url
-    assert_response :success
-  end
-
 end
--- test/controllers/front_controller_test.rb
+++ test/controllers/front_controller_test.rb
@@ -1,14 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class FrontControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get front_home_url
-    assert_response :success
-  end
-
-  test "should get search" do
-    get front_search_url
-    assert_response :success
-  end
-
 end
