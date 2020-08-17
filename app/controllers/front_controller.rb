e18713d713e0d2bb25548b25929fb705114b3d2f
Dássone J <dyotamo@gmail.com>
2020-08-17 13:42:44 +0200

    Styles...

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -6,18 +6,19 @@ class FrontController < ApplicationController
     @q = q.strip if !q.nil?
 
     redirect_to root_path if @q.empty?
-    @results = full_search(Company, @q, 7)
+    @results = full_search(Company, @q, 10)
   end
 
   private
 
   def full_search(model, query, page_number)
     rs = model.none
+    query = "%" + query + "%"
 
     # Evaluate the query time
-    @exc = Benchmark.measure {
-      rs = model.where("description like ?", "%" + query + "%")
-    }
+    @exc = Benchmark.measure do
+      rs = model.where("name like ? or category like ? or city like ? or description like ?", query, query, query, query)
+    end
 
     rs.paginate(page: params[:page], per_page: page_number)
   end
